8aa16c886e534d1465ebf4d3f24b18a9372687e7
Devoxin <lukel9914@gmail.com>
2020-05-16 16:09:58 +0100

    actually require a channel to prevent abuse

--- src/main/kotlin/gg/octave/bot/listeners/FlightEventAdapter.kt
+++ src/main/kotlin/gg/octave/bot/listeners/FlightEventAdapter.kt
@@ -200,7 +200,7 @@ class FlightEventAdapter : DefaultCommandEventAdapter() {
     companion object {
         fun isDJ(ctx: Context, send: Boolean = true): Boolean {
             val data = ctx.data
-            val isAlone = ctx.guild!!.audioManager.connectedChannel.let { it == null || it.members.count { m -> !m.user.isBot } == 1 }
+            val isAlone = ctx.guild!!.audioManager.connectedChannel.let { it != null && it.members.count { m -> !m.user.isBot } == 1 }
             val djRole = data.command.djRole
             val djRolePresent = djRole?.let(ctx.member!!::hasAnyRoleId)
                 ?: data.music.djRoles.any(ctx.member!!::hasAnyRoleId)
