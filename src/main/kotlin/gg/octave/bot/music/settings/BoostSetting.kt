fbf43a71f1e9f9e9babc9e8dccb917e25b655c0d
Austin Melms <memyselfandlie@gmail.com>
2020-05-17 15:54:09 -0400

    see melmsie for explanation

--- src/main/kotlin/gg/octave/bot/music/settings/BoostSetting.kt
+++ src/main/kotlin/gg/octave/bot/music/settings/BoostSetting.kt
@@ -5,5 +5,5 @@ enum class BoostSetting(val band1: Float, val band2: Float) {
     SOFT(0.25F, 0.15F),
     HARD(0.50F, 0.25F),
     EXTREME(0.75F, 0.50F),
-    EARRAPE(1F, 0.75F)
+    MINDBEND(1F, 0.75F)
 }
