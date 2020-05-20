50a8b6f26a4b368cf94801807ed975f022d1e3b6
kodehawa <david.alejandro.rubio@gmail.com>
2020-05-20 11:49:22 -0400

    guild_count and cached_users for NodeStatsPoster

--- src/main/kotlin/gg/octave/bot/apis/nodes/NodeInfoPoster.kt
+++ src/main/kotlin/gg/octave/bot/apis/nodes/NodeInfoPoster.kt
@@ -12,14 +12,18 @@ class NodeInfoPoster(var nodeId: Int) {
     fun postEvery(time: Long, unit: TimeUnit) {
         scheduler.scheduleWithFixedDelay({
             val ramUsedBytes = Runtime.getRuntime().let { it.totalMemory() - it.freeMemory() }
+            val ramTotal = Runtime.getRuntime().totalMemory()
 
             Launcher.database.jedisPool.resource.use {
                 it.hset("node-stats", nodeId.toString(),
                         JSONObject()
                                 .put("music_players", Launcher.players.size())
                                 .put("uptime", ManagementFactory.getRuntimeMXBean().uptime)
+                                .put("total_ram", ramTotal)
                                 .put("used_ram", ramUsedBytes)
                                 .put("thread_count", Thread.activeCount())
+                                .put("guild_count", Launcher.shardManager.guildCache.size())
+                                .put("cached_users", Launcher.shardManager.userCache.size())
                                 .toString()
                 )
             }
