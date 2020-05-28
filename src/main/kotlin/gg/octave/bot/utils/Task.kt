f21da20a4140217a705a6c8c83aa4f32d9a62ae1
Devoxin <lukel9914@gmail.com>
2020-05-28 23:01:55 +0100

    A little bit safer [nobuild]

--- src/main/kotlin/gg/octave/bot/utils/Task.kt
+++ src/main/kotlin/gg/octave/bot/utils/Task.kt
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit
 class Task(private val delay: Long, private val unit: TimeUnit, private val runnable: () -> Unit) {
     @Volatile
     private var task: Future<*>? = null
-    val isRunning: Boolean get() = task != null && !task!!.isCancelled
+    val isRunning: Boolean get() = task?.let { !it.isCancelled } ?: false // null
 
     /**
      * Starts the task, cancelling the previous one if it exists.
